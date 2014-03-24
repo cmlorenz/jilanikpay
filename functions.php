f8dd43573f4641d392ec7dfce23acd3e1d4fbe28
Chelsea <cmlorenz@syr.edu>
2014-03-24 00:01:16 -0400

    frontpage

--- functions.php
+++ functions.php
@@ -105,7 +105,7 @@ add_action( 'init', 'jilanikpay_init' );
  * Admin Theme Options Menu
  */
 function jilanikpay_admin_menu() {
-    add_theme_page( 'Jila Nikpay Theme Options', 'Theme Options', 'edit_theme_options', 'c404-theme-options', 'jilanikpay_theme_options' );
+   add_theme_page( 'Jila Nikpay Theme Options', 'Theme Options', 'edit_theme_options', 'c404-theme-options', 'jilanikpay_theme_options' );
 }
 add_action( 'admin_menu', 'jilanikpay_admin_menu' );
 
@@ -122,29 +122,30 @@ function jilanikpay_theme_options() { ?>
 				<h3 class="hndle"><span>Featured Film</span></h3>
 				<div class="inside">
 					<p>Select film to be displayed on front page.</p>
-					<form action="<?php bloginfo('url'); ?>" method="get">
-   						<?php wp_dropdown_pages(array(
+					<form action="<?php bloginfo('url'); ?>" method="get"><?php
+						wp_dropdown_pages( array(
 							'selected'=> $film_options['featured_film'],
 							'name' => 'featured_film',
 							'show_option_none' => 'None',
 							'class' => 'postform jilanikpay-dropdown',
-							'post_type' => 'film')); ?>
-   					</form>
+							'post_type' => 'film'
+						) ); ?>
+   				</form>
 				</div>
 			</section>
-            <section id="footer-options" class="postbox"><?php
-			    $footer_options = get_option('jilanikpay_footer'); ?>
+         <section id="footer-options" class="postbox"><?php
+			   $footer_options = get_option('jilanikpay_footer'); ?>
 				<div title="Click to Toggle" class="handlediv"></div>
 				<h3 class="hndle"><span>Footer Options</span></h3>
 				<div class="inside">
 					<p class="howto">Fill in contact information.</p>
 					<form class="input-prepend">
-                        <label for="copyright">Copyright:</label>
-                        <input id="copyright" name="copyright" type="text" value="<?php echo esc_attr(stripcslashes($footer_options['copyright'])); ?>"/>
+                  <label for="copyright">Copyright:</label>
+                  <input id="copyright" name="copyright" type="text" value="<?php echo esc_attr(stripcslashes($footer_options['copyright'])); ?>"/>
 					</form>
 				</div><!--/.inside-->
 			</section><!--/#footer-options-->
-        </div><!--/#theme-options-frame-->
+      </div><!--/#theme-options-frame-->
 	</div><!--/.wrap-->
 	<div class="submit-wrap">
 		<p class="submit">
@@ -159,11 +160,11 @@ function jilanikpay_theme_options() { ?>
 				//get form values individually
 				var values = {
 					"jilanikpay_film" :{
-                        "featured_film" : $("#featured_film option:selected").val(),
-                    },
+               	"featured_film" : $("#featured_film option:selected").val(),
+               },
 					"jilanikpay_footer" :{
-                        "copyright" : $('#copyright').val()
-                    }
+               	"copyright" : $('#copyright').val()
+               }
 				};
 				var data = {
 					action: 'jilanikpay_theme_options_ajax_action',
@@ -185,52 +186,48 @@ function jilanikpay_theme_options() { ?>
  * Theme Options Save
  */
 function jilanikpay_theme_options_ajax_action() {
-    $return = 'Changes Saved';
-    //update options
-    foreach( $_POST['options'] as $key => $value ) :
-        $changed = update_option( $key, $value );
-        if ( $changed ) :
-            $return = 'reload';
-        endif;
-    endforeach;
-    echo $return;
-    die();
+	$return = 'Changes Saved';
+	//update options
+	foreach( $_POST['options'] as $key => $value ) :
+		$changed = update_option( $key, $value );
+		if ( $changed ) :
+			$return = 'reload';
+		endif;
+	endforeach;
+	echo $return;
+	die();
 }
 add_action( 'wp_ajax_jilanikpay_theme_options_ajax_action', 'jilanikpay_theme_options_ajax_action' );
 
 /*
  * Meta Boxes
  */
-
 function jilanikpay_metabox() {                              
-    add_meta_box( 'embed-metabox', 'Film Embed Code', 'embed_metabox', 'film', 'normal', 'high' );
-    add_meta_box( 'gallery-metabox', 'Attached Gallery', 'gallery_metabox', 'photo', 'normal', 'high' );
-
+	add_meta_box( 'embed-metabox', 'Film Embed Code', 'embed_metabox', 'film', 'normal', 'high' );
+	add_meta_box( 'gallery-metabox', 'Attached Gallery', 'gallery_metabox', 'photo', 'normal', 'high' );
 }
 add_action( 'add_meta_boxes', 'jilanikpay_metabox' );
 
 function embed_metabox( $post ) {
-    $values = get_post_custom( $post->ID );
-    $selected = isset( $values['film_embed'] ) ? $values['film_embed'][0] : '';
-
-    wp_nonce_field( 'my_meta_box_nonce', 'meta_box_nonce' );
-    ?>
-    <p>
-        <label for="film_embed"><p>Paste embed code for the film in the textbox below.</p></label>
-        <textarea name="film_embed" id="film_embed" cols="62" rows="5" ><?php echo $selected; ?></textarea>
-    </p>
-    <?php   
+	$values = get_post_custom( $post->ID );
+	$selected = isset( $values['film_embed'] ) ? $values['film_embed'][0] : '';
+	wp_nonce_field( 'my_meta_box_nonce', 'meta_box_nonce' ); ?>
+	<p>
+		<label for="film_embed"><p>Paste embed code for the film in the textbox below.</p></label>
+		<textarea name="film_embed" id="film_embed" cols="62" rows="5" ><?php echo $selected; ?></textarea>
+	</p><?php   
 }
 
 function gallery_metabox ( $post ) { ?>
-	<p><strong>Use this option to set a featured gallery.</strong></p>
-	<?php wp_dropdown_categories( array(
-			'taxonomy'=>'gallery',
-			'selected'=> get_post_meta($post->ID, 'jilanikpay_gallery', true),
-			'name' => 'jilanikpay_gallery',
-			'show_option_none' => 'None',
-			'class' => 'postform jilanikpay-dropdown',
-			'hide_empty' => false) ); ?>
+	<p><strong>Use this option to set a featured gallery.</strong></p><?php
+	wp_dropdown_categories( array(
+		'taxonomy'=>'gallery',
+		'selected'=> get_post_meta($post->ID, 'jilanikpay_gallery', true),
+		'name' => 'jilanikpay_gallery',
+		'show_option_none' => 'None',
+		'class' => 'postform jilanikpay-dropdown',
+		'hide_empty' => false
+	) ); ?>
 	<script type="text/javascript">
 		jQuery(document).ready(function($){
 			$(".jilanikpay-dropdown").change(function(){
@@ -242,25 +239,23 @@ function gallery_metabox ( $post ) { ?>
 			});
 		});
 	</script>
-
 <?php }
 
 function metabox_save( $post_id ) {
-    if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
-    if( !isset( $_POST['meta_box_nonce'] ) || !wp_verify_nonce( $_POST['meta_box_nonce'], 'my_meta_box_nonce' ) ) return;
-    if( !current_user_can( 'edit_post' ) ) return;
+	if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
+	if( !isset( $_POST['meta_box_nonce'] ) || !wp_verify_nonce( $_POST['meta_box_nonce'], 'my_meta_box_nonce' ) ) return;
+	if( !current_user_can( 'edit_post' ) ) return;
 
-    $allowed = array( 
-        'a' => array( 
-            'href' => array() 
-        )
-    );
-    
-    if( isset( $_POST['film_embed'] ) )
-        update_post_meta( $post_id, 'film_embed', $_POST['film_embed'] );
+	$allowed = array( 
+		'a' => array( 
+			'href' => array() 
+		)
+	);
 
-    if( isset($_POST['jilanikpay_gallery']) )
-		update_post_meta($post_id, 'jilanikpay_gallery', $_POST['jilanikpay_gallery'] );
+	if( isset( $_POST['film_embed'] ) )
+		update_post_meta( $post_id, 'film_embed', $_POST['film_embed'] );
 
+	if( isset($_POST['jilanikpay_gallery']) )
+		update_post_meta( $post_id, 'jilanikpay_gallery', $_POST['jilanikpay_gallery'] );
 }
 add_action( 'save_post', 'metabox_save' );
