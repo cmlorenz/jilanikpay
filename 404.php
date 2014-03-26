0f4c4520c420c1aa1e7f67a1ec3673cc7be78cfd
Chelsea <cmlorenz@syr.edu>
2014-03-26 00:50:21 -0400

    404

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 404 (Page Not Found) Template
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ */
+get_header(); ?>
+
+<div id="notfound-content" class="site-content" role="main">
+	<h2 class="page-title"><?php _e( 'Not Found', 'twentyfourteen' ); ?></h2>
+	<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyfourteen' ); ?></p>
+	<?php get_search_form(); ?>
+</div><!-- #notfound-contents --><?php
+
+get_footer();
