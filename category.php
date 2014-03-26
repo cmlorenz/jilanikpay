044718052a1c7da4367b4376cce990e0d3c74cc6
Chelsea <cmlorenz@syr.edu>
2014-03-26 00:59:08 -0400

    category page

---
+++ category.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Category Template
+ *
+ * @package Twenty_Fourteen
+ * @subpackage Jila Nikpay
+ */
+get_header();
+$term_description = term_description(); ?>
+
+<div id="blog-content" class="site-content" role="main"><?php
+	if ( have_posts() ) : ?>
+		<h2 class="archive-title"><?php printf( __( 'Category Archives: %s', 'twentyfourteen' ), single_cat_title( '', false ) ); ?></h2><?php
+		if ( !empty( $term_description ) ) :
+			//printf( '<div class="taxonomy-description">%s</div>', $term_description );
+		endif; ?>
+		<div id="cat-feed"><?php
+			while ( have_posts() ) : the_post(); ?>
+				<article id="post-<?php the_ID(); ?>" class="blog-post">
+					<h3><?php echo get_the_date(); ?></h3>
+					<h2><?php the_title(); ?></h2>
+					<h4>By <?php the_author(); ?></h4>
+					<?php the_content(); ?>
+					<h5><?php the_tags(); ?></h5>
+				</article><?php
+			endwhile; ?>
+		</div><!-- #cat-feed --><?php
+		get_sidebar();
+	else : ?>
+		<h2 class="archive-title"><?php _e( 'Nothing Found', 'twentyfourteen' ); ?></h2>
+		<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'twentyfourteen' ); ?></p><?php
+		get_search_form();
+	endif; ?>
+</div><!-- #blog-content --><?php
+
+get_footer();
