bbf44ba5c599f32e4d08900a9cd981796d2a2667
Chelsea <cmlorenz@syr.edu>
2014-03-30 17:36:22 -0400

    style bugs

--- front-page.php
+++ front-page.php
@@ -9,8 +9,10 @@ get_header();
 $film = get_option('jilanikpay_film'); ?>
 
 <article id="frontpage-content" class="site-content" role="main">
-	<?php echo get_the_post_thumbnail($film['featured_film'], 'feat-film'); ?>
-	<h2><?php echo get_the_title($film['featured_film']); ?></h2>
+	<a class="img-link" href="<?php echo get_the_permalink($film['featured_film']); ?>">
+		<?php echo get_the_post_thumbnail($film['featured_film'], 'feat-film'); ?>
+		<h2><?php echo get_the_title($film['featured_film']); ?></h2>
+	</a>
 </article><!-- #frontpage-content --><?php
 
 get_footer();
