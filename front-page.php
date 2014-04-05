8f000fb900fe19b5e1c2111e5f30dd1637452268
Tori Holcomb <vgholcom@syr.edu>
2014-04-04 23:58:02 -0400

    permalink

--- front-page.php
+++ front-page.php
@@ -9,7 +9,7 @@ get_header();
 $film = get_option('jilanikpay_film'); ?>
 
 <article id="frontpage-content" class="site-content" role="main">
-	<a class="img-link" href="<?php echo get_the_permalink($film['featured_film']); ?>">
+	<a class="img-link" href="<?php echo get_permalink($film['featured_film']); ?>">
 		<?php echo get_the_post_thumbnail($film['featured_film'], 'feat-film'); ?>
 		<h2><?php echo get_the_title($film['featured_film']); ?></h2>
 	</a>
