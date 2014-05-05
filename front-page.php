6802d67339ba663839a1e449fce3cd2be2159770
Chelsea <cmlorenz@syr.edu>
2014-05-05 12:45:54 -0400

    tagline

--- front-page.php
+++ front-page.php
@@ -8,10 +8,8 @@
 get_header();
 $film = get_option('jilanikpay_film'); ?>
 
-<article id="frontpage-content" class="site-content" role="main"><?php
-	while ( have_posts() ) : the_post();
-		the_content();
-	endwhile; wp_reset_postdata(); ?>
+<article id="frontpage-content" class="site-content" role="main">
+	<p><?php bloginfo( 'description' ); ?></p>
 	<a class="img-link" href="<?php echo get_permalink($film['featured_film']); ?>">
 		<?php echo get_the_post_thumbnail($film['featured_film'], 'feat-film'); ?>
 		<h2><?php echo get_the_title($film['featured_film']); ?></h2>
